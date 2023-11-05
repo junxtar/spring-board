9b3c0e20b8c7ca9b45be75053017555f35ac6516
junxtar <aa1870088@naver.com>
2023-11-05 19:46:29 +0900

    refactor: 게시판 title 필드 추가

--- src/main/java/com/sparta/springboard/domain/board/BoardEntity.java
+++ src/main/java/com/sparta/springboard/domain/board/BoardEntity.java
@@ -18,22 +18,31 @@ public class BoardEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;
     private String writer;
     private String content;
     private String password;
 
     @Builder
-    private BoardEntity(String writer, String content, String password) {
+    private BoardEntity(String title, String writer, String content, String password) {
+        this.title = title;
         this.writer = writer;
         this.content = content;
         this.password = password;
     }
 
-    public static BoardEntity create(String writer, String content, String password) {
+    public static BoardEntity create(String title, String writer, String content, String password) {
         return BoardEntity.builder()
+                .title(title)
                 .writer(writer)
                 .content(content)
                 .password(password)
                 .build();
+    }
+
+    public void update(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
     }
 }
